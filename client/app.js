ab1cfad20dabb3e54324444e179fb57df072a57c
Dominik Mast <dominik-mast@web.de>
2017-04-10 11:17:01 +0000

    little angular steps

--- client/app.js
+++ client/app.js
@@ -1,2 +1,12 @@
-var app = angular.module("VotingApp", []);
+var app = angular.module("VotingApp", ["ngRoute"]);
 
+ app.config(function($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl: '/views/home.html'
+    })
+    .when("/test", {
+        templateUrl: "/views/test.html"
+    })
+
+});
