82b7bacca9109d0f8eb1e51e41c5d1a72c6900b4
Dominik Mast <dominik-mast@web.de>
2017-04-11 10:39:54 +0000

    registrierung fertig

--- client/js/controllers/mainController.js
+++ client/js/controllers/mainController.js
@@ -1,12 +1,20 @@
 app.controller('MainController', ['$scope', "$location", "$http", function($scope,$location, $http) {
    
     $scope.go = function(path){
-        $location.path(path)
-    }
+        $location.path(path);
+    };
     
     $scope.newSubmit = function (newUserData){
-        // was nun?
-    }
+       if (!newUserData.name || !newUserData.email || !newUserData.password)
+       {$scope.regError= "Anmeldedaten sind Fehlerhaft"}
+       else{
+            $http.post("/signUp", newUserData).success(function(res){
+                if (res == true){$location.path("/userHome")}
+                else{$scope.regError = res}
+                
+            });
+       }
+    };
     
     $scope.loginSubmit = function(loginData){
         $http.post ("/login", loginData).success(function(res){
--- index.js
+++ index.js
@@ -2,16 +2,18 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
 const mongoClient = require("mongodb").MongoClient;
-const dburl = "mongodb://webapps:webapps@ds157500.mlab.com:57500/webapps"
+const dburl = "mongodb://webapps:webapps@ds157500.mlab.com:57500/webapps";
 
 
-app.use(express.static(__dirname+"/client"))
+app.use(express.static(__dirname+"/client"));
 app.use(bodyParser.json());
 
 /* Login vorgang*/
 app.post("/login", function(req,res){
     mongoClient.connect(dburl, function(err,db){
+        if(err) throw err;
         db.collection("userdata").find({}).toArray(function(err,result){
+            if (err) throw err;
             var login = "false";
             
             for(var i= 0; i<result.length; i++){
@@ -26,12 +28,51 @@ app.post("/login", function(req,res){
             res.send(login);
         });
         db.close();
-        
     });
-    
 });
 
+/* User Anlegen*/
+app.post("/signUp", function(req,res){
+    mongoClient.connect(dburl,function(err,db){
+        if (err) throw err;
+        
+        /* prüfen ob Name und Email schon vergeben sind */
+        db.collection("userdata").find({}).toArray(function(err,result){
+            if (err) throw err;
+            var check = true;
+    
+               for (var i= 0; i< result.length; i++){
+                  if (req.body.name.toLowerCase() == result[i].user){
+                      check = "Name ist bereits vergeben";
+                      break;
+                  }
+                  if (req.body.email.toLowerCase() == result[i].email){
+                      check = "Email ist bereits vergeben";
+                      break;
+                  }
+               }
+               
+               /* Name oder Email schon in Benutzung */
+               if (check !== "true"){
+                   res.send(check);
+                   db.close();
+               }
+                /* User Daten in die Db schreiben */
+               else{
+                    db.collection("userdata").insert({
+                        "user": req.body.name,
+                        "email": req.body.email,
+                        "password": req.body.password
+                    });
+                    
+                res.send(check);    
+                db.close();
+               }
+         });
+    });
+});
 
+/* Hier könnte es weiter gehen*/
 
 
 
